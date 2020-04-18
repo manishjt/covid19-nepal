3b987c9cb2a895a9f4d7e1552da9aa0108b0e6eb
Shreyasha <shreyashapdl@gmail.com>
2020-04-18 12:01:52 -0700

    Updates to seir. Inference in progress

--- seir/seir.py
+++ seir/seir.py
@@ -1,6 +1,18 @@
 import numpy as np
 
 def SEIR(x, M , pop, ts, pop0):
+    ''' Input description:
+        x: state variables for each ensemble (6*375, 300)
+        M: mobility matrix (375, 375, 14)
+        pop: population matrix (375, 1)
+        ts: timestep
+        pop0: inintial population
+        375 = number of nodes
+        300 = number of ensembles
+        6 = number of states
+    '''
+    
+
     #the metapopulation SEIR model
     dt = 1
     tmstep = 1
@@ -8,7 +20,7 @@ def SEIR(x, M , pop, ts, pop0):
     #integrate forward for one day
     num_loc = pop.shape[0];
     num_ens = x.shape[1];
-   
+
     #S,E,Is,Ia,obs,...,beta,mu,theta,Z,alpha,D
     #TODO: check 0 indexing
     Sidx = np.array(range(1,5,5*num_loc));
@@ -41,7 +53,7 @@ def SEIR(x, M , pop, ts, pop0):
     Z=x[Zidx,:];
     alpha=x[alphaidx,:];
     D=x[Didx,:];
-   
+
     #start integration
     #TODO: pythonize, rewrite repeated steps as functions
     tcnt=0;
@@ -54,7 +66,7 @@ def SEIR(x, M , pop, ts, pop0):
         Ia_tcnt = Ia[:,:,tcnt]
         Is_tcnt = Is[:,:,tcnt]
         E_tcnt = E[:, :, tcnt]
-        
+
         dt_theta = dt1*(np.ones(num_loc,1) * theta)
         temp2 = (pop-Is_tcnt)
         ESenter=np.multiply(dt_theta,
@@ -72,8 +84,8 @@ def SEIR(x, M , pop, ts, pop0):
         EIaleft=np.min(np.multiply(dt_theta,
                             np.multiply(np.divide(Ia_tcnt,temp2),(np.sum(M_ts).T*ones(1,num_ens)))),
                        dt1*Ia_tcnt);
-       
-        
+
+
         Eexps=np.divide(dt1*np.multiply((ones(num_loc,1)*beta),np.multiply(S_tcnt,Is_tcnt)),pop);
         Eexpa=np.divide(np.multiply(dt1*(ones(num_loc,1)*mu),
                                     np.multiply((ones(num_loc,1)*beta),
@@ -83,14 +95,14 @@ def SEIR(x, M , pop, ts, pop0):
         Einfa=np.divide(dt1*np.multiply((ones(num_loc,1)*(1-alpha)),E_tcnt),(ones(num_loc,1)*Z));
         Erecs=dt1*np.divide(Is_tcnt,(ones(num_loc,1)*D));
         Ereca=dt1*np.divide(Ia_tcnt,ones(num_loc,1)*D);
-       
+
         ESenter=max(ESenter,0);ESleft=max(ESleft,0);
         EEenter=max(EEenter,0);EEleft=max(EEleft,0);
         EIaenter=max(EIaenter,0);EIaleft=max(EIaleft,0);
         Eexps=max(Eexps,0);Eexpa=max(Eexpa,0);
         Einfs=max(Einfs,0);Einfa=max(Einfa,0);
         Erecs=max(Erecs,0);Ereca=max(Ereca,0);
-       
+
         ##########stochastic version
         ESenter=np.random.poisson(ESenter);ESleft=np.random.poisson(ESleft);
         EEenter=np.random.poisson(EEenter);EEleft=np.random.poisson(EEleft);
@@ -107,14 +119,14 @@ def SEIR(x, M , pop, ts, pop0):
         isk1=Einfs-Erecs;
         iak1=Einfa-Ereca+EIaenter-EIaleft;
         ik1i=Einfs;
-        
-        
+
+
         #second step
         Ts1=S[:,:,tcnt]+sk1/2;
         Te1=E[:,:,tcnt]+ek1/2;
         Tis1=Is[:,:,tcnt]+isk1/2;
         Tia1=Ia[:,:,tcnt]+iak1/2;
-       
+
         M_ts = M[:, :, ts]
         temp3 = pop-Tis1
         temp4 = (sum(M_ts).T*ones(1,num_ens))
@@ -124,7 +136,7 @@ def SEIR(x, M , pop, ts, pop0):
         EEleft=min(np.multiply(np.multiply(dt_theta,np.divide(Te1,temp3)),temp4),dt1*Te1);
         EIaenter=np.multiply(dt_theta,(M_ts*np.divide(Tia1,temp3)));
         EIaleft=min(np.multiply(np.multiply(dt_theta,np.divide(Tia1,temp3)),temp4),dt1*Tia1);
-       
+
         num_loc_ones = np.ones(num_loc, 1)
         Eexps=np.multiply(dt1*(num_loc_ones*beta),
                           np.multiply(Ts1, np.divide(Tis1,pop)));
@@ -135,14 +147,14 @@ def SEIR(x, M , pop, ts, pop0):
         Einfa=np.multiply(dt1*(num_loc_ones*(1-alpha)),np.divide(Te1,(num_loc_ones*Z)));
         Erecs=dt1*np.divide(Tis1,(num_loc_ones*D));
         Ereca=dt1*np.divide(Tia1,(num_loc_ones*D));
-       
+
         ESenter=max(ESenter,0);ESleft=max(ESleft,0);
         EEenter=max(EEenter,0);EEleft=max(EEleft,0);
         EIaenter=max(EIaenter,0);EIaleft=max(EIaleft,0);
         Eexps=max(Eexps,0);Eexpa=max(Eexpa,0);
         Einfs=max(Einfs,0);Einfa=max(Einfa,0);
         Erecs=max(Erecs,0);Ereca=max(Ereca,0);
-       
+
         ###########stochastic version
         ESenter=np.random.poisson(ESenter);ESleft=np.random.poisson(ESleft);
         EEenter=np.random.poisson(EEenter);EEleft=np.random.poisson(EEleft);
@@ -159,13 +171,13 @@ def SEIR(x, M , pop, ts, pop0):
         isk2=Einfs-Erecs;
         iak2=Einfa-Ereca+EIaenter-EIaleft;
         ik2i=Einfs;
-        
+
         #third step
         Ts2=S[:,:,tcnt]+sk2/2;
         Te2=E[:,:,tcnt]+ek2/2;
         Tis2=Is[:,:,tcnt]+isk2/2;
         Tia2=Ia[:,:,tcnt]+iak2/2;
-       
+
         M_ts = M[:, :, ts]
         temp5 = pop-Tis2
         temp6 = (sum(M_ts).T*ones(1,num_ens))
@@ -175,7 +187,7 @@ def SEIR(x, M , pop, ts, pop0):
         EEleft=min(np.multiply(np.multiply(dt_theta,np.divide(Te2,temp5)),temp6),dt1*Te2);
         EIaenter=np.multiply(dt_theta,(M_ts*np.divide(Tia2,temp5)));
         EIaleft=min(np.multiply(np.multiply(dt_theta,np.divide(Tia2,temp5)),temp6),dt1*Tia2);
-       
+
         num_loc_ones = np.ones(num_loc, 1)
         Eexps=np.multiply(dt1*(num_loc_ones*beta),
                           np.multiply(Ts2, np.divide(Tis2,pop)));
@@ -187,14 +199,14 @@ def SEIR(x, M , pop, ts, pop0):
         Erecs=dt1*np.divide(Tis2,(num_loc_ones*D));
         Ereca=dt1*np.divide(Tia2,(num_loc_ones*D));
 
-       
+
         ESenter=max(ESenter,0);ESleft=max(ESleft,0);
         EEenter=max(EEenter,0);EEleft=max(EEleft,0);
         EIaenter=max(EIaenter,0);EIaleft=max(EIaleft,0);
         Eexps=max(Eexps,0);Eexpa=max(Eexpa,0);
         Einfs=max(Einfs,0);Einfa=max(Einfa,0);
         Erecs=max(Erecs,0);Ereca=max(Ereca,0);
-       
+
         ###########stochastic version
         ESenter=np.random.poisson(ESenter);ESleft=np.random.poisson(ESleft);
         EEenter=np.random.poisson(np.random.poissonEEenter);EEleft=np.random.poisson(EEleft);
@@ -211,13 +223,13 @@ def SEIR(x, M , pop, ts, pop0):
         isk3=Einfs-Erecs;
         iak3=Einfa-Ereca+EIaenter-EIaleft;
         ik3i=Einfs;
-       
+
         #fourth step
         Ts3=S[:,:,tcnt]+sk3;
         Te3=E[:,:,tcnt]+ek3;
         Tis3=Is[:,:,tcnt]+isk3;
         Tia3=Ia[:,:,tcnt]+iak3;
-        
+
         M_ts = M[:, :, ts]
         temp5 = pop-Tis3
         temp6 = (sum(M_ts).T*ones(1,num_ens))
@@ -227,7 +239,7 @@ def SEIR(x, M , pop, ts, pop0):
         EEleft=min(np.multiply(np.multiply(dt_theta,np.divide(Te3,temp5)),temp6),dt1*Te3);
         EIaenter=np.multiply(dt_theta,(M_ts*np.divide(Tia3,temp5)));
         EIaleft=min(np.multiply(np.multiply(dt_theta,np.divide(Tia3,temp5)),temp6),dt1*Tia3);
-       
+
         num_loc_ones = np.ones(num_loc, 1)
         Eexps=np.multiply(dt1*(num_loc_ones*beta),
                           np.multiply(Ts3, np.divide(Tis3,pop)));
@@ -245,7 +257,7 @@ def SEIR(x, M , pop, ts, pop0):
         Eexps=max(Eexps,0);Eexpa=max(Eexpa,0);
         Einfs=max(Einfs,0);Einfa=max(Einfa,0);
         Erecs=max(Erecs,0);Ereca=max(Ereca,0);
-       
+
         ##########stochastic version
         ESenter=np.random.poisson(ESenter);ESleft=np.random.poisson(ESleft);
         EEenter=np.random.poisson(EEenter);EEleft=np.random.poisson(EEleft);
@@ -262,7 +274,7 @@ def SEIR(x, M , pop, ts, pop0):
         isk4=Einfs-Erecs;
         iak4=Einfa-Ereca+EIaenter-EIaleft;
         ik4i=Einfs;
-       
+
         ##########
         S[:,:,tcnt+1]=S[:,:,tcnt]+round(sk1/6+sk2/3+sk3/3+sk4/6);
         E[:,:,tcnt+1]=E[:,:,tcnt]+round(ek1/6+ek2/3+ek3/3+ek4/6);
@@ -270,7 +282,7 @@ def SEIR(x, M , pop, ts, pop0):
         Ia[:,:,tcnt+1]=Ia[:,:,tcnt]+round(iak1/6+iak2/3+iak3/3+iak4/6);
         Incidence[:,:,tcnt+1]=round(ik1i/6+ik2i/3+ik3i/3+ik4i/6);
         obs=Incidence[:,:,tcnt+1];
-    
+
     ##update x
     x[Sidx,:]=S[:,:,tcnt+1];
     x[Eidx,:]=E[:,:,tcnt+1];
@@ -281,5 +293,5 @@ def SEIR(x, M , pop, ts, pop0):
     pop=pop-np.sum(M[:,:,ts],1).T*theta+np.sum(M[:,:,ts],2)*theta;
     minfrac=0.6;
     pop[pop<minfrac*pop0]=pop0[pop<minfrac*pop0]*minfrac;
-                                                                      
+
     return x, pop
