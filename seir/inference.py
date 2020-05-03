3fde37a54f4b2fce2912ef1df5e975f5d8264ce7
Shreyasha <shreyashapdl@gmail.com>
2020-05-03 11:25:54 -0700

    Bugfixes: indexing and typos

--- seir/inference.py
+++ seir/inference.py
@@ -6,63 +6,62 @@ def inference(M, pop, incidence):
 #Inference for the metapopulation SEIR model
 #Adapted from code by Sen Pei
 
-   #Load files
-   # mobility (373,375,14)
-   # population (375, 1)
-   # load observation (14, 375)
+    #Load files
+    # mobility (373,375,14)
+    # population (375, 1)
+    # load observation (14, 375)
 
-   # Define constants
-   Td=9;                         #average reporting delay
-   a=1.85;                       # shape parameter of gamma distribution
-   b=Td/a;                       # scale parameter of gamma distribution
+    # Define constants
+    Td=9;                         # average reporting delay
+    a=1.85;                       # shape parameter of gamma distribution
+    b=Td/a;                       # scale parameter of gamma distribution
 
-   num_loc=M.shape[0];           # number of locations
-   rnds=ceil(gamrnd(a,b,1e4,1)); # pre-generate gamma random numbers
+    num_loc=M.shape[0];           # number of locations
+    rnds=np.ceil(np.random.gamma(a,b,(10000,1))); # pre-generate gamma random numbers
 
-   # observation operator: obs=Hx
-   H=np.zeros([num_loc,5*num_loc+6]);
-   for i in range(num_loc):
+    # observation operator: obs=Hx
+    H=np.zeros([num_loc,5*num_loc+6]);
+    for i in range(num_loc):
        H[i,(i-1)*5+5]=1;
 
 
-   num_times=incidence.shape[0];
-   obs_truth=incidence.T;
-   #set OEV
-   OEV=np.zeros([num_loc,num_times]);
-   for l in range(num_loc):
-       for t in range(num_times):(
-           OEV[l,t] = max(4,obs_truth[l,t]^2/4);
-       end
-   end
+    num_times=incidence.shape[0];
+    obs_truth=incidence.T;
 
-   num_ens=300;#number of ensemble
-   pop0=pop*np.ones([1,num_ens]);
-   [x,paramax,paramin]=initialize(pop0,num_ens);#get parameter range
-   num_var=x.shape[0];#number of state variables
+    #set OEV
+    OEV=np.zeros([num_loc,num_times]);
+    for l in range(num_loc):
+       for t in range(num_times):
+           OEV[l,t] = max(4,obs_truth[l,t]**2/4);
 
-   #IF setting
-   Iter=10; #number of iterations
-   num_para=paramax.shape[0];#number of parameters
-   theta=np.zeros([num_para,Iter+1]);#mean parameters at each iteration
-   para_post=np.zeros([num_para,num_ens,num_times,Iter]);#posterior parameters
-   sig=np.zeros([1,Iter]);#variance shrinking parameter
-   alp=0.9;#variance shrinking rate
-   SIG=np.power((paramax-paramin),2/4);#initial covariance of parameters
-   lambda_val=1.1;#inflation parameter to aviod divergence within each iteration
+    num_ens=300;#number of ensemble
+    pop0=pop*np.ones([1,num_ens]);
+    [x,paramax,paramin]=initialize(pop0,num_ens, M);#get parameter range
+    num_var=x.shape[0];#number of state variables
 
-   %start iteration for Iter round
-   for n in range(Iter):
-       sig[n]=alp^(n-1);
+    # IF setting
+    Iter=10; #number of iterations
+    num_para=paramax.shape[0];#number of parameters
+    theta=np.zeros([num_para,Iter+1]);#mean parameters at each iteration
+    para_post=np.zeros([num_para,num_ens,num_times,Iter]);#posterior parameters
+    sig=np.zeros([1,Iter]);#variance shrinking parameter
+    alp=0.9;#variance shrinking rate
+    SIG=np.power((paramax-paramin),2/4);#initial covariance of parameters
+    lambda_val=1.1;#inflation parameter to aviod divergence within each iteration
+
+    #start iteration for Iter round
+    for n in range(Iter):
+       sig[n]=alp**(n-1);
        #generate new ensemble members using multivariate normal distribution
-       Sigma=np.diag(sig[n]^2*SIG);
+       Sigma=np.diag(sig[n]**2*SIG);
        if (n==1):
            #first guess of state space
-           [x,~,~]=initialize(pop0,num_ens);
+           [x,_,_]=initialize(pop0,num_ens);
            para=x[end-5:end,:];
            theta[:,1]=np.mean(para,2);#mean parameter
-       else
-           [x,~,~]=initialize(pop0,num_ens);
-           para=np.random.multivariate_normal(theta(:,n).T,Sigma,num_ens).T;#generate parameters (multivariate random numbers)
+       else:
+           [x,_, _]=initialize(pop0,num_ens);
+           para =np.random.multivariate_normal(theta[:,n].T,Sigma,num_ens).T;#generate parameters (multivariate random numbers)
            x[end-5:,:]=para;
 
        #correct lower/upper bounds of the parameters
@@ -87,7 +86,7 @@ def inference(M, pop, incidence):
                    if obs_cnt[l,k]>0:
                        rnd=datasample[rnds,obs_cnt[l,k]];
                        for h in range(len(rnd)):
-                           if (t+rnd[h]<=num_times)
+                           if (t+rnd[h]<=num_times):
                                obs_temp[l,k,t+rnd[h]]=obs_temp[l,k,t+rnd[h]]+1;
 
 
@@ -103,8 +102,8 @@ def inference(M, pop, incidence):
                prior_var = np.var(obs_ens[l,:]);
                post_var = prior_var*obs_var/(prior_var+obs_var);
                if prior_var==0:#if degenerate
-                   post_var=1e-3;
-                   prior_var=1e-3;
+                   post_var = 1e-3;
+                   prior_va = 1e-3;
 
                prior_mean = np.mean(obs_ens[l,:]);
                post_mean = post_var*(prior_mean/prior_var + obs_truth[l,t]/obs_var);
@@ -139,7 +138,7 @@ def inference(M, pop, incidence):
            x_post[:,:,t]=x;
            para_post[:,:,t,n]=x[end-5:,:];
 
-       para=x_post(end-5:end,:,1:num_times);
+       para=x_post[-5:,:,1:num_times];
        temp=np.squeeze(np.mean(para,2));#average over ensemble members
        theta[:,n+1]=np.mean(temp,2);#average over time
 
